c40dfe44f288f79173ef827bf2c9f9f051d75ea1
noahthetriche <noahthetriche@gmail.com>
2020-03-10 14:00:57 -0700

    completed both methods and prints properly -noah

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -12,19 +12,22 @@ public class BubbleSort
 
     void bubbleSort(int arr[])
         {
+            int b = arr.length -1;
+
             for(int i = 0; i < arr.length; i++)
             {
-                for(int i2 = 0; i2 < arr.length; i++)
+                for(int i2 = 0; i2 < b; i2++)
                 {
-                    int temp = 0;
+                    int temp;
 
-                    if(arr[i] > arr[i + 1])
+                    if(arr[i2] > arr[i2 + 1])
                     {
-                        temp = arr[i];
-                        arr[i] = arr[i+1];
-                        arr[i + 1] = temp;
+                        temp = arr[i2];
+                        arr[i2] = arr[i2+1];
+                        arr[i2 + 1] = temp;
                     }
                 }
+                b--;
             }
 
         }
@@ -32,6 +35,10 @@ public class BubbleSort
         /* Prints the array */
         void printArray(int arr[])
         {
+            for(int i = 0; i < arr.length; i++)
+            {
+                System.out.println(arr[i]);
+            }
 
         }
 
