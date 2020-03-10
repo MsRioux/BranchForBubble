3153ae62fd269c1d170c429ebe4874700f4a01bc
thefluffyoshi <katielynntrinh@gmail.com>
2020-03-10 13:56:37 -0700

    nested for loops, if statement

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -8,17 +8,36 @@ public class BubbleSort
         ob.bubbleSort(arr);
         System.out.println("Sorted array");
         ob.printArray(arr);
-    }
+    }//end main
 
     void bubbleSort(int arr[])
         {
+            int temp;
+            //nested for loops
+            //swaps
+            //compare 2 ints, if a > b switch
+            //right most/highest stays after iteration
+            //when done 1st iteration check again (2nd iteration)
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
 
-        }
+                }//end nested for loop
+            }//end outer for loop
+
+            if (arr[0] > arr[1])
+            {
+                temp = arr[0];
+                arr[0] = arr[1];
+                arr[1] = temp;
+            }//end if
+        }//end bubbleSort
 
         /* Prints the array */
         void printArray(int arr[])
         {
 
-        }
+        }//end array print
 
 }//end class BubbleSort
