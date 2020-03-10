7ecc0eeb4b156c9c98300ebd398163cb59b6621a
BowDown097 <btd6modsyeet@gmail.com>
2020-03-10 13:43:22 -0700

    Finish bubbleSort()

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -13,7 +13,18 @@ public class BubbleSort
 
     void bubbleSort(int arr[])
     {
-
+        for(int i = 0; i < arr.length; i++)
+        {
+            for(int j = 0; j < arr.length-1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    arr[j] ^= arr[j + 1];
+                    arr[j + 1] ^= arr[j];
+                    arr[j] ^= arr[j + 1];
+                }
+            }
+        }
     }
 
     /* Prints the array  */
