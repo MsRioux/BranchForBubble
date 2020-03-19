f0ff4204f9e2f76681447948a9ff0ea04bd6f1cb
rokie95 <motiurga@gmail.com>
2020-03-18 19:04:55 -0700

    finished bubbleSort method and reformatted

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -11,16 +11,28 @@ public class BubbleSort
     }
 
     void bubbleSort(int arr[])
+    {
+        for(int j = 0; j < 100; j++) 
         {
-
-        }
+	    	for(int i = 0; i < arr.length-1; i++) 
+            {
+	    		if(arr[i] < arr[i+1]) 
+                {
+	    			int temp = arr[i];
+	    			arr[i] = arr[i+1];
+	    			arr[i+1] = temp;
+	    		}
+	    	}
+    	}
+    }
 
         /* Prints the array */
-        void printArray(int arr[])
+    void printArray(int arr[])
+    {
+        for(int i = 0; i < arr.length; i++) 
         {
-            for(int i = 0; i < arr.length; i++) {
-    		    System.out.print(arr[i] + " ");
-    	    }
+            System.out.print(arr[i] + " ");
         }
+    }
 
 }//end class BubbleSort
