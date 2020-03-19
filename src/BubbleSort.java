92a82b6edb16c641131f50f302b179bd5086d5fd
coliner300 <54290215+coliner300@users.noreply.github.com>
2020-03-19 11:16:12 -0700

    Finished bubbleSort algorithm

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,6 +1,5 @@
 public class BubbleSort
 {
-
     public static void main(String args[])
     {
         BubbleSort ob = new BubbleSort();
@@ -12,13 +11,22 @@ public class BubbleSort
 
     void bubbleSort(int arr[])
         {
-
+            for (int i = 0; i < arr.length - 1; i++)
+            {
+                for (int j = 0; j < arr.length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j+1])
+                    {
+                        int temp = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = temp;
+                    } 
+                } 
+            } 
         }
-
         /* Prints the array */
         void printArray(int arr[])
         {
-
+            
         }
-
-}//end class BubbleSort
+}
