64418dfa714b96ee96bb49ddc74ff5f3ea384e2f
chewbecca615 <54290152+chewbecca615@users.noreply.github.com>
2020-03-19 16:27:15 -0700

    finished bubbleSort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -7,15 +7,24 @@ public class BubbleSort
         ob.bubbleSort(arr);
         System.out.println("Sorted array");
         ob.printArray(arr);
-        System.out.println("bec was here");
-    }
+    } // end of main method
     void bubbleSort(int arr[])
     {
-
-    }
-    /* Prints the array */
+        for (int i = 0; i < arr.length - 1; i++)
+        {
+            for (int j = 0; j < arr.length - i - 1; j++)
+            {
+                if (arr[j] > arr[j+1])
+                {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                } // end of if
+            } // end of for loop for j
+        } // end of for loop for i
+    } // end of bubbleSort
     void printArray(int arr[])
     {
-
-    }
-}//end class BubbleSort
+         
+    } // end of printarray
+} // end of class
