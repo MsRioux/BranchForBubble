7d7b0451653cb779e90a3ea112fb5b33df8a99b2
chewbecca615 <54290152+chewbecca615@users.noreply.github.com>
2020-03-19 16:42:12 -0700

    finished printarray

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -25,6 +25,9 @@ public class BubbleSort
     } // end of bubbleSort
     void printArray(int arr[])
     {
-         
+        for (int num : arr)
+        {
+            System.out.print(num + "    ");
+        } // end of for loop to print
     } // end of printarray
 } // end of class
