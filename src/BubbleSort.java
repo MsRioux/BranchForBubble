ec6d98bbca6a54e7f95c0e3245f14cd9599a5742
Nerdof2004 <nerdof2004@gmail.com>
2020-03-20 15:40:12 -0700

    Added Print Added Bubble sort algorithm completed Added print array completed

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -8,17 +8,33 @@ public class BubbleSort
         ob.bubbleSort(arr);
         System.out.println("Sorted array");
         ob.printArray(arr);
+        System.out.println("Yeeeet");
     }
 
     void bubbleSort(int arr[])
+    {
+        int temp;
+        for (int i= 0; i< arr.length- 1; i++)
         {
+            for (int j= 0; j< arr.length- i - 1; j++)
+            {
+                if (arr[j] > arr[j+1])
+                {
+                    temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        } // end of for loop
+    } // end of bubbleSort
 
-        }
-
-        /* Prints the array */
-        void printArray(int arr[])
+    /* Prints the array */
+    void printArray(int arr[])
+    {
+        for(int num = 0; num < arr.length; num++)
         {
-
-        }
+            System.out.println(arr[num]);
+        }// end of for loop
+    }// end of print array
 
 }//end class BubbleSort
