52878d0a61cdf9bee11392c48d750b0c5baa9cfe
matsunei <54189313+matsunei@users.noreply.github.com>
2020-03-20 19:41:37 -0700

    Update BubbleSort.java

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -10,15 +10,28 @@ public class BubbleSort
         ob.printArray(arr);
     }
 
-    void bubbleSort(int arr[])
+   void bubbleSort(int arr[])
         {
-
-        }
+            for (int i = 0; i < arr.length - 1; i++)
+            {
+                for (int j = 0; j < arr.length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j+1])
+                    {
+                        int temp = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = temp;
+                    } // end if
+                } // emd inner if
+            } // end outer if
+        } // end method bubblesort
 
         /* Prints the array */
         void printArray(int arr[])
         {
-
-        }
-
+            for (int num : arr)
+            {
+                System.out.print(num + "    ");
+            } // end for
+        } // end method printArray
 }//end class BubbleSort
