1b80268f86153bc90004ee7060eccca252290ccd
Er1c02 <54290100+Er1c02@users.noreply.github.com>
2020-03-20 23:02:27 -0700

    Update BubbleSort.java

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -4,15 +4,7 @@ public class BubbleSort
     {
         BubbleSort ob = new BubbleSort();
         int arr[] = {36, 23, 115,212, 42, 12, 42};
-        ob.bubbleSort(arr);
-        System.out.println("Sorted array");
-        ob.printArray(arr);
-    } // end of main method
-    void bubbleSort(int arr[])
-    {
-        for (int i = 0; i < arr.length - 1; i++)
-        {
-            for (int j = 0; j < arr.length - i - 1; j++)
+        ob.bubbleSongth - i - 1; j++)
             {
                 if (arr[j] > arr[j+1])
                 {
