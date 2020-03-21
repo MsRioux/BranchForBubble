822bb4d8443f53fe72bda4ef9eafcdd6482ccc40
Alejandrag002 <54290129+Alejandrag002@users.noreply.github.com>
2020-03-20 19:08:24 -0700

    Update BubbleSort.java

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -13,13 +13,27 @@ public class BubbleSort
 
     void bubbleSort(int arr[])
         {
-
+            for (int i = 0; i < arr.length - 1; i++)
+             {
+              for (int j = 0; j < arr.length - i - 1; j++)
+               {
+                 if (arr[j] > arr[j+1])
+                   {
+                      int temp = arr[j + 1];
+                      arr[j + 1] = arr[j];
+                       arr[j] = temp;
+                   } // endif
+               } // endloopj
+             } // endloopi
         }
 
         /* Prints the array */
         void printArray(int arr[])
         {
-
+           for (int num : arr)
+            {
+                System.out.print(num + "    ");
+            } // end loop
         }
 
 }//end class BubbleSort
