29b8847106d606d8d00cd7f9844602953162a4be
AMorales5796 <54290204+AMorales5796@users.noreply.github.com>
2020-03-22 00:20:23 -0700

    Completed

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -12,13 +12,28 @@ public class BubbleSort
 
     void bubbleSort(int arr[])
         {
-
+            int tempPos;//this is a temporary variable that allows two other variables to swap
+            for(int i = 0; i < arr.length; i++)//act as first element
+            {
+                for(int j = 0; j < arr.length-1; j++)//act as second/consectutive element
+                {
+                    if(arr[j] > arr[j+1])//checking if the first element is greater than the consecutive element
+                    {
+                        tempPos = arr[j+1];//this is swapping the consecutive element to be placed where the first element is
+                        arr[j+1] = arr[j]; //thus the "second" element will be first
+                        arr[j] = tempPos;  //and the "first" will be "second"
+                    }//end of if conditional
+                }//end of inner for loop
+            } //end of outer for loop
         }
 
         /* Prints the array */
         void printArray(int arr[])
         {
-
+            for(int i = 0; i < arr.length; i++)
+            {
+                System.out.print(arr[i]);
+            }
         }
 
 }//end class BubbleSort
